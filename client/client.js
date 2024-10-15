1c0e4557d06aa81ec1e41af2905b38f96f31c77d
mdrhm <mdtrahman04@gmail.com>
2024-10-15 19:25:45 -0400

    Some basic socket.io functionality

--- client/client.js
+++ client/client.js
@@ -1,1 +1,22 @@
 const socket = io();
+const createGame = document.querySelector("#create-game")
+
+createGame.addEventListener("click", () =>{
+    socket.emit("createGame")
+})
+
+const roomID = new URLSearchParams(window.location.search).get('room')
+
+if (roomID) {
+    playerName = prompt("What's your name?")
+    socket.emit("joinGame", data = {roomID: roomID, playerName: playerName})
+}
+
+socket.on("updatePlayers", (data) => {
+    const playersList = document.querySelector("#players-list")
+    playersList.innerText = data["players"].join("\n")
+});
+
+socket.on("gameCreated", (data) => {
+    window.location.href = "?room=" + data["roomID"]
+})
--- server.js
+++ server.js
@@ -10,11 +10,37 @@ const rooms = {}; // Store room information
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/index.html'));
 });
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 io.on('connection', (socket) => {
     console.log('a user has connected');
+    socket.on('createGame', () => {
+        const roomID = makeid(6)
+        rooms[roomID] = {players: []}
+        socket.join(roomID);
+        socket.emit("gameCreated", {roomID: roomID})
+    })
+    socket.on('joinGame', (data) => {
+        if (rooms[data.roomID]) {
+            socket.join(data.roomID);
+            rooms[data.roomID]["players"].push(data["playerName"]);
+            io.to(data.roomID).emit('updatePlayers', rooms[data.roomID]);
+        }
+        else {
+            console.log("This room doesn't exist")
+        }
+    });
 })
 
 server.listen(3000, () => {
