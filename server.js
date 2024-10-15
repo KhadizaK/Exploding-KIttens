5fe31e7b0cfadb2524a3dc5b8e4eb025435a4869
mdrhm <mdtrahman04@gmail.com>
2024-10-15 18:46:26 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const rooms = {}; // Store room information
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
