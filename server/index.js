2d911928133fd046fe1de6bf3c8bc5edf9d3a554
Khadiza Khanom <khadiza.s.khanom@gmail.com>
2024-11-05 22:43:58 -0500

    Added eslint to server folder

--- server/index.js
+++ server/index.js
@@ -9,22 +9,21 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("send_message", (data) => {
-        console.log(data);
-        socket.broadcast.emit("receive_message", data);
-    });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
 });
 
 server.listen(3001, () => {
-    console.log("SERVER IS RUNNING  ");
+  console.log("SERVER IS RUNNING  ");
 });
-
