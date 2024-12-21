8ac25361ad7d98ecfcad2f4f66b7b859a37ba7dc
zehdi02 <de.melchizedek.castro@gmail.com>
2024-12-21 17:09:28 -0500

    Added sound effect upon clicking Button.js components

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import useSound from 'use-sound';
 import { motion } from "motion/react";
+import buttonClickSound from '../assets/sounds/button_click.mp3'; // Make sure this path is correct
 
 const Button = ({ title, link, onClick }) => {
+    const [play] = useSound(buttonClickSound);
+
+    const handleClick = () => {
+        play(); // Play the click sound
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return link ? (
         // Render as a link if a 'link' prop is provided
         <motion.div
@@ -20,7 +31,7 @@ const Button = ({ title, link, onClick }) => {
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
         >
-            <button onClick={onClick}
+            <button onClick={handleClick}
                 className='Button rounded-xl shadow-xl w-max px-4 py-1 hover:brightness-90'
                 style={{ backgroundColor: '#4E4C4D', color: '#EC3631' }}>
                 {title}
